ff3637dba70149b001308e391679ce8018180a1c
Matt <m.ray.varkki@gmail.com>
2020-02-02 00:33:45 +0100

    Added Feedback, when Boopables were thrown

---
+++ Assets/_Project/Scripts/WobbleIt.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+public class WobbleIt : MonoBehaviour
+{
+    public float duration = 1;
+    public Vector3 strength = new Vector3(1,1,1);
+    public int vibrato = 18;
+    [Range(0F,80F)]public float randomness = 50F;
+    
+    public void Wobble()
+    {
+        transform.DOShakeScale(duration, strength, vibrato, randomness, true);
+    }
+}
