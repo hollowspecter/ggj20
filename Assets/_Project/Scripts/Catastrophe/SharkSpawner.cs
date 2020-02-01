cf9f596919ad0167656f69ae2942c650886bccfa
Vivien Baguio <baguiov@posteo.net>
2020-02-01 21:06:24 +0100

    setup sharknado scipts

---
+++ Assets/_Project/Scripts/Catastrophe/SharkSpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SharkSpawner : MonoBehaviour
+{
+    public GameObject sharkPrefab;
+    public Vector2 spawnDurationMinMax = new Vector2(2f, 8f);
+
+    private float nextSharkSpawnTime;
+
+    private void Start()
+    {
+        ResetSpawnTimer();
+    }
+
+    private void Update()
+    {
+        if (Time.time > nextSharkSpawnTime)
+        {
+            ResetSpawnTimer();
+            SpawnShark();
+        }
+    }
+
+    private void ResetSpawnTimer()
+    {
+        nextSharkSpawnTime = Random.Range(spawnDurationMinMax.x, spawnDurationMinMax.y);
+    }
+
+    private void SpawnShark()
+    {
+        // spawn shark
+
+        // take a good angle to shoot
+
+        // shoot with force
+    }
+}
