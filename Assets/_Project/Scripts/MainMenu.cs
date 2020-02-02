8b88a887c6a49d519f57a1742f6c3b957183cac8
Matt <m.ray.varkki@gmail.com>
2020-02-02 13:11:20 +0100

    Updating MainMenu

--- Assets/_Project/Scripts/MainMenu.cs
+++ Assets/_Project/Scripts/MainMenu.cs
@@ -1,18 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public string mainSceneName;
+    
+    void StartGame()
     {
-        
+        // SceneManager.LoadScene(mainSceneName, LoadSceneMode.Single);
+    }
+    
+    void ExitGame()
+    {
+        Application.Quit();
     }
 }
