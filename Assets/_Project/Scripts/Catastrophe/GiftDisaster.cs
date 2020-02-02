5da5d531f36aae2311c7a468e5ad5817a2280a50
Vivien Baguio <baguiov@posteo.net>
2020-02-02 11:07:04 +0100

    Gift Disaster: is holding onto the gift

--- Assets/_Project/Scripts/Catastrophe/GiftDisaster.cs
+++ Assets/_Project/Scripts/Catastrophe/GiftDisaster.cs
@@ -9,11 +9,16 @@ public class GiftDisaster : MonoBehaviour
     public UnityEvent onGoodGiftReceived;
     public UnityEvent onBadGiftReceived;
 
+    private Rigidbody giftRigidbody;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.TryGetComponent<Attachable>(out var gift))
         {
             Debug.Log("Received a gift! " + gift.name);
+            Destroy(gift); // get rid of attachable
+            other.attachedRigidbody.isKinematic = true;
+            giftRigidbody = other.attachedRigidbody;
 
             if (goodGifts.Contains(gift.gameObject))
             {
@@ -23,6 +28,14 @@ public class GiftDisaster : MonoBehaviour
             {
                 onBadGiftReceived?.Invoke();
             }
+        }
+    }
+
+    public void LetGoOfGift()
+    {
+        if (giftRigidbody != null)
+        {
+            giftRigidbody.isKinematic = false;
         }
     }
 }
